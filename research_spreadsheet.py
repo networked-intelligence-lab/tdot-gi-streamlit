66724bcd4680689b251f83d796cc1d7473a0e58f
Dax Ledesma <dakilaledesma@gmail.com>
2023-06-28 13:19:00 -0400

    Research spreadsheet Streamlit conversion

--- research_spreadsheet.py
+++ research_spreadsheet.py
@@ -1,7 +1,41 @@
 import streamlit as st
 import pandas as pd
+from collections import defaultdict
 import numpy as np
 
 st.title('Research Spreadsheet')
 spreadsheet_t = st.tabs(["Spreadsheet Tool"])
 
+option_df = pd.read_excel("data/ResearchProjectSpreadsheet.xlsx", sheet_name="DesignConsiderations")
+categories_df = option_df[["Unnamed: 0", "Subcategory"]][3:]
+
+category_dict = defaultdict(dict)
+curr_category = None
+for index, row in categories_df.iterrows():
+    category = row["Unnamed: 0"]
+    subcategory = row["Subcategory"]
+    if category != curr_category and str(category) != "nan":
+        curr_category = category
+
+    if str(subcategory) != "nan":
+        category_dict[curr_category][subcategory] = index
+    else:
+        category_dict[curr_category][category] = index
+
+major_category = st.selectbox("Major Category", [k for k in category_dict.keys()])
+sub_dict = category_dict[major_category]
+subcategory = st.selectbox("Subcategory", [k for k in sub_dict.keys()])
+
+for col in option_df.columns[2:]:
+    col_header = [v for v in option_df[col][:3] if str(v) != "nan"]
+    headers = []
+    for idx in range(3, -1, -1):
+        h_val = idx + 2
+        try:
+            headers.append(f'<h{h_val}>{col_header[idx]}</h{h_val}>')
+        except IndexError:
+            pass
+    headers.reverse()
+
+    st.write("".join(headers), unsafe_allow_html=True)
+    st.markdown(f"<p>{option_df[col][category_dict[major_category][subcategory]]}</p>", unsafe_allow_html=True)
