8290301b8595646119d3d7bce83d4c1d849b4042
Dax Ledesma <37383659+dakilaledesma@users.noreply.github.com>
2023-07-29 15:37:14 -0400

    Push site logo through add_logo from st-extras

--- home.py
+++ home.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from streamlit_js_eval import streamlit_js_eval, get_geolocation
+from streamlit_extras.app_logo import add_logo
 import pandas as pd
+
+add_logo("media/logo.png", height=150)
 
 st.title('TDoT GI Home')
 loc = get_geolocation()
