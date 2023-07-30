bd409cd4c1984606c30784c9e77c76ff16cd7021
Dax Ledesma <37383659+dakilaledesma@users.noreply.github.com>
2023-07-30 12:38:08 -0400

    Initial push of extended retention basin code

--- pages/cost_tools.py
+++ pages/cost_tools.py
@@ -2,8 +2,15 @@ import streamlit as st
 from pages.ct_scripts import *
 
 st.title('Cost Tool')
-cistern_st, bioretention_st, swales_st = st.tabs(["Cistern", "Bioretention", "Swales"])
+(cistern_st,
+ bioretention_st,
+ ed_basin_st, 
+ swales_st) = st.tabs(["Cistern",
+                       "Bioretention",
+                       "Extended Detention Basin",
+                       "Swales"])
 
 cisterns_tab(cistern_st)
 swales_tab(swales_st)
+ed_basin_tab(ed_basin_st)
 bioretention_tab(bioretention_st)
--- pages/ct_scripts/__init__.py
+++ pages/ct_scripts/__init__.py
@@ -1,3 +1,4 @@
 from pages.ct_scripts.cisterns import cisterns_tab
 from pages.ct_scripts.swales import swales_tab
 from pages.ct_scripts.bioretention import bioretention_tab
+from pages.ct_scripts.ed_basin import ed_basin_tab
