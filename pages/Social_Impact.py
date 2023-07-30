a69be0f3ba187281439b0024dc361e17b20a39c5
Dax Ledesma <37383659+dakilaledesma@users.noreply.github.com>
2023-07-30 16:32:07 -0400

    Implement Google Maps places API for nearby landmarks and assoc UI elements for options

--- pages/Social_Impact.py
+++ pages/Social_Impact.py
@@ -2,22 +2,49 @@ import streamlit as st
 from streamlit_js_eval import streamlit_js_eval, get_geolocation
 import pandas as pd
 from streamlit_extras.app_logo import add_logo
+import googlemaps
+import pprint
+import json
+import geopy.distance
+
+with open("tokens/tokens") as tokens_file:
+    tokens = json.load(tokens_file)
 
 add_logo("media/logo.png", height=150)
-
 st.title("Social Impact")
 
 loc = get_geolocation()
 if loc:
-    loc_df = pd.DataFrame([[loc["coords"]["latitude"], loc["coords"]["longitude"]]], columns=["lat", "lon"])
     loc_input = st.text_input("Enter latitude and longitude", value=f"{loc['coords']['latitude']}, {loc['coords']['longitude']}")
+    print(loc_input)
+    loc_df = pd.DataFrame([[float(loc_input.split(", ")[0]), float(loc_input.split(", ")[1]), "#FF000090"]],
+                          columns=["lat", "lon", "color"])
 else:
     loc_input = st.text_input("Enter latitude and longitude", value="36.1627, -86.7816")
-    loc_df = pd.DataFrame([[float(loc_input.split(",")[0]), float(loc_input.split(",")[1])]], columns=["lat", "lon"])
+    print(loc_input)
+    loc_df = pd.DataFrame([[float(loc_input.split(",")[0]), float(loc_input.split(",")[1]), "#FF000090"]], columns=["lat", "lon", "color"])
 st.write("Enter latitude and longitude in the format: 36.1627, -86.7816")
 
+st.subheader("Nearby Parks")
+radius_input = st.slider("Radius (miles)", min_value=0.1, max_value=30.0, value=0.5)
+gmaps = googlemaps.Client(key=tokens["gmaps"])
+user_loc = (loc_df.iloc[0]["lat"], loc_df.iloc[0]["lon"])
+nearby_result = gmaps.places_nearby(
+    location=user_loc,
+    radius=radius_input * 1609.344,
+    keyword="park")
+
+pp = pprint.PrettyPrinter(indent=1)
+parks_list = []
+for place_result in nearby_result["results"]:
+    place_loc = (place_result["geometry"]["location"]["lat"], place_result["geometry"]["location"]["lng"])
+    loc_df = loc_df.append({"lat": place_loc[0], "lon": place_loc[1], "color": "#0000FF90"}, ignore_index=True)
+    parks_list.append({"Distance Away (straight-line; in miles)": geopy.distance.distance(user_loc, place_loc).miles, "Park Name": place_result["name"]})
+parks_df = pd.DataFrame.from_dict(parks_list).sort_values(by="Distance Away (straight-line; in miles)", ascending=True)
+st.dataframe(parks_df, hide_index=True)
+
 st.header("User Location")
-st.map(data=loc_df)
+st.map(loc_df, latitude="lat", longitude="lon", color="color", size=20)
 
 st.header("Enhanced Property Value")
 median_prop_val = st.number_input("Median of the property value for that area and anticipated enhancement in value", value=0)
