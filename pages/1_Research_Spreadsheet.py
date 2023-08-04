5345dcb94e7c4af58ce4006253a372953208d3ab
Dax Ledesma <37383659+dakilaledesma@users.noreply.github.com>
2023-08-04 15:07:52 -0400

    Add read logic and col for possible GI output

--- pages/1_Research_Spreadsheet.py
+++ pages/1_Research_Spreadsheet.py
@@ -2,27 +2,50 @@ import streamlit as st
 import pandas as pd
 from collections import defaultdict
 
-st.title('Research Spreadsheet')
-spreadsheet_t = st.tabs(["Spreadsheet Tool"])
+col1, col2 = st.columns(2)
 
 option_df = pd.read_excel("data/ResearchProjectSpreadsheet.xlsx", sheet_name="DesignConsiderations")
-for col in option_df.columns[2:]:
-    col_header = [v for v in option_df[col][:3] if str(v) != "nan"]
-    headers = []
-    for idx in range(3, -1, -1):
-        h_val = idx + 4
-        try:
-            headers.append(f'<h{h_val}>{col_header[idx]}</h{h_val}>')
-        except IndexError:
-            pass
-    headers.reverse()
-    options_text = headers.pop().split(">")[1].split("<")[0]
+categories_df = option_df[["Unnamed: 0", "Subcategory"]][3:]
+category_dict = defaultdict(dict)
+curr_category = None
+for index, row in categories_df.iterrows():
+    category = row["Unnamed: 0"]
+    subcategory = row["Subcategory"]
+    if category != curr_category and str(category) != "nan":
+        curr_category = category
 
-    st.write("".join(headers), unsafe_allow_html=True)
-    options = list(set([str(v) for v in option_df[col][3:] if str(v) != "nan"]))
-    options.insert(0, "")
-    options.sort()
-    st.selectbox(options_text, options)
+    if str(subcategory) != "nan":
+        category_dict[curr_category][subcategory] = index
+    else:
+        category_dict[curr_category][category] = index
+
+with col1:
+    st.title('Research Spreadsheet')
+    spreadsheet_t = st.tabs(["Spreadsheet Tool"])
+
+    for col in option_df.columns[2:]:
+        col_header = [v for v in option_df[col][:3] if str(v) != "nan"]
+        headers = []
+        for idx in range(3, -1, -1):
+            h_val = idx + 4
+            try:
+                headers.append(f'<h{h_val}>{col_header[idx]}</h{h_val}>')
+            except IndexError:
+                pass
+        headers.reverse()
+        options_text = headers.pop().split(">")[1].split("<")[0]
+
+        st.write("".join(headers), unsafe_allow_html=True)
+        options = list(set([str(v) for v in option_df[col][3:] if str(v) != "nan"]))
+        options.insert(0, "")
+        options.sort()
+        st.selectbox(options_text, options)
+
+with col2:
+    st.subheader("Possible Green Infrastructure")
+    for major_category in category_dict.keys():
+        sub_dict = category_dict[major_category]
+        st.multiselect(major_category, [k for k in sub_dict.keys()], [k for k in sub_dict.keys()])
 
 # categories_df = option_df[["Unnamed: 0", "Subcategory"]][3:]
 #
